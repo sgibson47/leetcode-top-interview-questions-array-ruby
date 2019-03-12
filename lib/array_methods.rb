a9eab8daad1419e930b4f22fafe61b0b9ce78b11
sgibson47 <sgibson47@gmail.com>
2019-03-12 14:24:46 -0500

    add solution for #max_profits

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,3 +3,41 @@ def remove_duplicates(nums)
 
   nums.length
 end
+
+def max_profit(prices)
+  #if there's only one day of prices, you can't profit
+  return 0 if prices.length < 2
+  
+  #keep track of whether you own stock
+  own_share = false
+  #keep track of your profit
+  profit = 0
+
+  # loop through each price
+  i = 0
+  while i <prices.length - 1 do
+    # if the price is going up tomorrow, 
+    # and you don't already own
+    # buy
+    if prices[i] < prices[i+1] && !own_share
+      profit -= prices[i]
+      own_share = true
+    end
+    # if the price is going down tomorrow, 
+    # and you own
+    # sell
+    if prices[i] > prices[i+1] && own_share
+      profit += prices[i]
+      own_share = false
+    end
+    #increment the counter each loop 
+    #to move on to the next day's price
+    i += 1
+  end
+
+  #if you own at the end, sell 
+  profit += prices[i] if own_share 
+
+  #return profit
+  profit
+end
